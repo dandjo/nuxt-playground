9d7787271747f47c2de9e689606f6cfab2b33a1b
Daniel Pernold <daniel.pernold@gmail.com>
2018-09-13 13:26:44 +0200

    New: Add sources

---
+++ pages/article/_id.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ name }}: {{ id }}</h1>
+</template>
+
+<script>
+  export default {
+    validate({params}) {
+      // Must be a number
+      return /^\d+$/.test(params.id)
+    },
+    asyncData(context) {
+      // Called every time before loading the component
+      return {name: 'Article', id: context.params.id}
+    },
+    fetch() {
+      // The `fetch` method is used to fill the store before rendering the page
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Hello my friend!</h1>
+        Consider visiting our article page at <nuxt-link to="/article/99">Article 99</nuxt-link>
+    </div>
+</template>
