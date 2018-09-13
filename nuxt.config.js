06b8718a4ea804dbf234d419392816da685d4453
Daniel Pernold <daniel.pernold@gmail.com>
2018-09-13 20:28:03 +0200

    Fix: Remove export breaking stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,12 +7,3 @@ module.exports = {
     // proxyHeaders: false
   }
 }
-
-export default {
-  head: {
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-    ]
-  }
-}
